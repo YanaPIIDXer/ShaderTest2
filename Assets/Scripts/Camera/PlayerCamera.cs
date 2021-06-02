2d41c8bd4d3f2bc2e2c821c85d23b20d2ac2d26a
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-06-03 08:47:25 +0900

    カメラを一応実装

---
+++ Assets/Scripts/Camera/PlayerCamera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Camera
+{
+    /// <summary>
+    /// プレイヤーカメラ
+    /// </summary>
+    public class PlayerCamera : MonoBehaviour
+    {
+        /// <summary>
+        /// プレイヤーのTransform
+        /// </summary>
+        [SerializeField]
+        private Transform PlayerTransform = null;
+
+        /// <summary>
+        /// プレイヤーからのオフセット座標
+        /// </summary>
+        private Vector3 PositionOffset = new Vector3(0.0f, 1.0f, -2.0f);
+
+        void FixedUpdate()
+        {
+            transform.position = PlayerTransform.position + PositionOffset;
+        }
+    }
+}
