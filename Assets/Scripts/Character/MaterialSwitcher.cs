2d5fae9556f34dfb162fe1f0a68975b193c839fa
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-06-03 09:28:32 +0900

    マテリアルを切り替える機構を作成

---
+++ Assets/Scripts/Character/MaterialSwitcher.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace Character
+{
+    /// <summary>
+    /// マテリアルセット
+    /// </summary>
+    [Serializable]
+    public class MaterialSet
+    {
+        [SerializeField]
+        public Material Body = null;
+        [SerializeField]
+        public Material Hair = null;
+        [SerializeField]
+        public Material Cheek = null;
+        [SerializeField]
+        public Material Skin = null;
+    }
+
+    /// <summary>
+    /// マテリアル切り替え
+    /// </summary>
+    public class MaterialSwitcher : MonoBehaviour
+    {
+        /// <summary>
+        /// 初期マテリアルセット
+        /// </summary>
+        [SerializeField]
+        private MaterialSet DefaultMaterialSet = new MaterialSet();
+
+        /// <summary>
+        /// Bodyマテリアルをアサインする対象リスト
+        /// </summary>
+        [SerializeField]
+        private List<SkinnedMeshRenderer> BodyMaterialTargets = new List<SkinnedMeshRenderer>();
+
+        /// <summary>
+        /// Hairマテリアルをアサインする対象リスト
+        /// </summary>
+        [SerializeField]
+        private List<SkinnedMeshRenderer> HairMaterialTargets = new List<SkinnedMeshRenderer>();
+
+        /// <summary>
+        /// Cheekマテリアルをアサインする対象
+        /// </summary>
+        [SerializeField]
+        private SkinnedMeshRenderer CheekMaterialTarget = null;
+
+        /// <summary>
+        /// Skinマテリアルをアサインする対象
+        /// </summary>
+        [SerializeField]
+        private SkinnedMeshRenderer SkinMaterialTarget = null;
+
+        void Awake()
+        {
+            AssignDefaultMaterial();
+        }
+
+        /// <summary>
+        /// デフォルトのマテリアルをアサイン
+        /// </summary>
+        private void AssignDefaultMaterial()
+        {
+            AssignMaterialSet(DefaultMaterialSet);
+        }
+
+        /// <summary>
+        /// 指定したマテリアルセットをアサイン
+        /// </summary>
+        /// <param name="Set">マテリアルセット</param>
+        private void AssignMaterialSet(MaterialSet Set)
+        {
+            foreach (var r in BodyMaterialTargets)
+            {
+                r.material = Set.Body;
+            }
+
+            foreach (var r in HairMaterialTargets)
+            {
+                r.material = Set.Hair;
+            }
+
+            CheekMaterialTarget.material = Set.Cheek;
+            SkinMaterialTarget.material = Set.Skin;
+        }
+    }
+}
